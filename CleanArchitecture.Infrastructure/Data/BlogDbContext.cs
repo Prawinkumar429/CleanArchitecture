0e51f714a2afd89cc89d55a8f1f6277426742874
PERUKULLA <PERUKULLA@ATS-LT035>
2025-11-03 10:19:23 +0530

    Add project files.

---
+++ CleanArchitecture.Infrastructure/Data/BlogDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        { 
+        }
+            public DbSet<Blog> Blogs { get; set;}
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Repositories/BlogRepository.cs
@@ -0,0 +1,58 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interface;
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly BlogDbContext _blogDbContext;
+
+        public BlogRepository(BlogDbContext blogDbContext)
+        {
+            _blogDbContext = blogDbContext;
+        }
+        public async Task<Blog> CreateAsync(Blog blog)
+        {
+            await _blogDbContext.Blogs.AddAsync(blog);
+            await  _blogDbContext.SaveChangesAsync();
+            return blog;
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            return await _blogDbContext.Blogs
+                .Where(b => b.Id == id)
+                .ExecuteDeleteAsync();
+        }
+
+        public async Task<List<Blog>> GetAllAsync()
+        {
+            return await _blogDbContext.Blogs.ToListAsync();
+        }
+
+        public async Task<Blog> GetByIdAsync(int id)
+        {
+            return await _blogDbContext.Blogs.AsNoTracking()
+                .FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<int> UpdateAsync(int id, Blog blog)
+        {
+            return await _blogDbContext.Blogs
+                .Where(b => b.Id == id)
+                .ExecuteUpdateAsync(b => b
+                    .SetProperty(p => p.Name, blog.Name)
+                    .SetProperty(p => p.Description, blog.Description)
+                    .SetProperty(p => p.Author, blog.Author)
+                    .SetProperty(p => p.ImageUrl, blog.ImageUrl)
+                );
+        }
+    }
+}
